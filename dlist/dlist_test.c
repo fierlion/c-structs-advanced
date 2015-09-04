e9aa73e27bed1632f152fee2cd221c2ef6c05460
Ray Allan <fierlion@gmail.com>
2015-09-03 17:56:22 -0700

    added to glist_test.c using glib test framework

--- dlist/dlist_test.c
+++ dlist/dlist_test.c
@@ -1,2 +1,26 @@
 #include <glib.h>
 #include "dlist.c"
+
+int *create_int(int data) {
+  int *value = malloc(sizeof(data));
+  memset(value, 0, sizeof(data));
+  *value = data;
+  return(value);
+}
+
+void destroy_int(void *data) {
+  free((int *)data);
+}
+
+typedef struct {
+  DList *dd;
+} dfixture;
+
+void dlist_setup(dfixture *df, gconstpointer test_data) {
+  df->dd = malloc(sizeof(DList));
+  dlist_init(df->dd, &destroy_int);
+}
+
+void dlist_teardown(dfixture *df, gconstpointer test_data) {
+  dlist_destroy(df->dd);
+}
